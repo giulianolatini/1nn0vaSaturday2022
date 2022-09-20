1474219d53d4b6051f43039fabcaef72bda0f43a
Giuliano Latini <giuliano.latini@prxm.it>
2022-09-20 17:05:24 +0200

    Complete Demo

--- Demo/Get-ApiResult.ps1
+++ Demo/Get-ApiResult.ps1
@@ -4,5 +4,14 @@ function Get-ApiResult {
         [PSCustomObject] $WriteOut
     )
     
-    return $true
+    $result = Invoke-RestMethod -Uri $ReadIn.URL -Method $ReadIn.Verb -Body $ReadIn.Payload -ErrorAction Stop
+    $WriteOut.HttpStatus = $result.StatusCode
+    $WriteOut.Payload = $result.Content
+    $WriteOut.Message = $result.StatusDescription
+    
+    if ($null -ne $WriteOut.Payload) {
+        return $true
+    } else {
+        return $false
+    }
 }
