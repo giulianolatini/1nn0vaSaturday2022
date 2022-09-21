b76d6b1fa759efda0fdb1ff7564d28055bd95706
Giuliano Latini <giuliano.latini@prxm.it>
2022-09-21 21:01:05 +0200

    Complete Demo

--- Demo/Get-ApiKeyValue.ps1
+++ Demo/Get-ApiKeyValue.ps1
@@ -4,6 +4,50 @@ function Get-ApiKeyValue {
         [PSCustomObject] $WriteOut
     )
 
-    return $true
+    function ConvertPSObjectToHashtable
+    {
+        param (
+            [Parameter(ValueFromPipeline)]
+            $InputObject
+        )
     
+        process
+        {
+            if ($null -eq $InputObject) { return $null }
+    
+            if ($InputObject -is [System.Collections.IEnumerable] -and $InputObject -isnot [string])
+            {
+                $collection = @(
+                    foreach ($object in $InputObject) { ConvertPSObjectToHashtable $object }
+                )
+    
+                Write-Output -NoEnumerate $collection
+            }
+            elseif ($InputObject -is [psobject])
+            {
+                $hash = @{}
+    
+                foreach ($property in $InputObject.PSObject.Properties)
+                {
+                    $hash[$property.Name] = ConvertPSObjectToHashtable $property.Value
+                }
+    
+                $hash
+            }
+            else
+            {
+                $InputObject
+            }
+        }
+    }
+
+    $result = Invoke-RestMethod -Uri $ReadIn.URL -ErrorAction Stop
+
+    $WriteOut.HttpStatus = $result.StatusCode
+
+    $hashresult = ConvertPSObjectToHashtable $result.Content.metadata
+    $WriteOut.keyValue  = $hashresult[$ReadIn.key]
+
+    $WriteOut.Message = $result.StatusDescription
+
 }
